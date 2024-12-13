e377d96b8af32f0ce519e19b7b043a6782527043
bot473gaming <tungnguyenduong473@gmail.com>
2024-12-14 00:34:57 +0700

    ok123

--- main.py
+++ main.py
@@ -11,14 +11,11 @@ class App(ctk.CTk):
         self.title("Quản lí đơn hàng")
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
-            
+
         # Tạo menu bên trái
         self.menu_frame = ctk.CTkFrame(self, width=250, corner_radius=0)
         self.menu_frame.grid(row=0, column=0, sticky="ns")
-        self.menu_frame.grid_rowconfigure(5, weight=1)
-        
-        # separator = ctk.CTkFrame(self, width=2, fg_color="gray")  # Dòng kẻ mỏng với màu xám
-        # separator.grid(row=0, column=0)   
+        self.menu_frame.grid_rowconfigure(4, weight=1)
 
         menu_label = ctk.CTkLabel(self.menu_frame, text="Menu", font=ctk.CTkFont(size=20, weight="bold"))
         menu_label.grid(row=0, column=0, padx=20, pady=(20, 10))
@@ -32,12 +29,16 @@ class App(ctk.CTk):
         customers_button = ctk.CTkButton(self.menu_frame, text="Báo cáo doanh thu", command=lambda: self.show_frame(ReportDT))
         customers_button.grid(row=3, column=0, padx=20, pady=10)
 
+
         # Tạo frame chứa nội dung
         self.content_frame = ctk.CTkFrame(self)
         self.content_frame.grid(row=0, column=1, sticky="nsew")
-        self.content_frame.grid_rowconfigure(0, weight=1)  # Chiều cao linh hoạt
-        self.content_frame.grid_columnconfigure(0, weight=1)  # Chiều rộng linh hoạt
+        self.content_frame.grid_rowconfigure(0, weight=1)  # Flexible height
+        self.content_frame.grid_columnconfigure(0, weight=1)  # Flexible width
 
+        # Tạo viền bên trái cho menu
+        self.left_border = ctk.CTkFrame(self, width=5, corner_radius=0, fg_color="gray")  # Viền bên trái của menu
+        self.left_border.grid(row=0, column=1, sticky="nsw", padx=0, pady=0)  # Gắn viền vào lề trái của menu
         # Khởi tạo các trang
         self.frames = {}
         for F in (CreateOrder, ManagerProducts, ReportDT):
@@ -45,18 +46,18 @@ class App(ctk.CTk):
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(CreateOrder)  # Hiển thị trang đầu tiên
+        self.show_frame(CreateOrder)  # Show the first page
 
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
-    
-    # Nếu là trang báo cáo doanh thu, tự động cập nhật khi chuyển đến trang này
+
+        # Auto-update when switching to ReportDT page
         if isinstance(frame, ReportDT):
             frame.update_data()
-        elif (isinstance(frame, CreateOrder)):
+        elif isinstance(frame, CreateOrder):
             frame.update_search_results()
-        elif (isinstance(frame, ManagerProducts)):
+        elif isinstance(frame, ManagerProducts):
             frame.load_products()
 
 if __name__ == "__main__":
